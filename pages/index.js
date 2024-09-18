a0d8dfa116bd6fc0b7bed58570172e3762b7d15e
lilblueyes <101664783+lilblueyes@users.noreply.github.com>
2024-09-18 13:05:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -100,11 +100,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Accéléromètre App avec GPS</title>
-        <meta
-          name="description"
-          content="Accelerometer x GPS"
-        />
+        <title>Accelerometer x GPS</title>
+        <meta name="description" content="Accelerometer x GPS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
